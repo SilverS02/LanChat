598cc2ee39ddf96ac55fe121337ab1f2a84b96a7
Alejandro <sanchezbarboza12345@gmail.com>
2021-07-16 09:56:45 -0400

    Renombrado de la carpeta _class a util

---
+++ src/util/User.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Larry Finol
+ */
+public class User implements Serializable{
+
+    private String name, ip;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setIP(String ip) {
+        this.ip = ip;
+    }
+
+    public String getIP() {
+        return this.ip;
+    }
+}
